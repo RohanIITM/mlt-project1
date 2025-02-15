84e3f26369b69e0bb54674b06aa6baf96ba18c79
RohanIITM <23f1000271@ds.study.iitm.ac.in>
2025-02-15 20:53:29 +0530

    feat: updated error handling

--- app.py
+++ app.py
@@ -11,16 +11,16 @@ app = FastAPI()
 def run_task(task: str):
     try:
         eval_task(task)
-        return {"status": "success"}
-    except ValueError:
-        raise HTTPException(status_code=400, detail="Invalid task request")
-    except Exception:
-        raise HTTPException(status_code=500, detail="Internal server error")
+        return {"ran": task}
+    except ValueError as exc:
+        raise HTTPException(status_code=400, detail=f"Invalid task request {exc}")
+    except Exception as exc:
+        raise HTTPException(status_code=500, detail=f"Server error: {exc}")
 
 
 @app.get("/read")
 def read_path(path: str):
     file_path = Path(path)
     if not file_path.exists():
-        raise HTTPException(status_code=404, detail="File not found")
+        raise HTTPException(status_code=404, detail=f"File: {file_path} not found!")
     return file_path.read_text()
